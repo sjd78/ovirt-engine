ce9f04ecce3eb558cb7022e5762719d4929e6f8c
Scott J Dickerson <sdickers@redhat.com>
2022-05-02 15:32:41 -0400

    Refactor spec build to use --with and --without to make it configurable in copr (and koji) builds

--- .automation/build-srpm.sh
+++ .automation/build-srpm.sh
@@ -1,45 +1,30 @@
 #!/bin/bash -xe
 
-# git hash of current commit should be passed as the 1st paraameter
+# Ensure the build validation passes
+make validations
+
+# git hash of current commit from GH action or from the HEAD commit
 if [ "${GITHUB_SHA}" == "" ]; then
-  GIT_HASH=$(git rev-list HEAD | wc -l)
+  GIT_HASH=$(git rev-parse --short HEAD)
 else
   GIT_HASH=$(git rev-parse --short $GITHUB_SHA)
 fi
 
-# Directory, where build artifacts will be stored, should be passed as the 1st parameter
-ARTIFACTS_DIR=${1:-exported-artifacts}
+# Prepare the RELEASE string (same work as patching `{?release_suffix}` in .copr/Makefile)
+export RELEASE=".git${GIT_HASH}"
+export MILESTONE=master
 
-# Prepare the version string (with support for SNAPSHOT versioning)
-VERSION=$(mvn help:evaluate  -q -DforceStdout -Dexpression=project.version)
-VERSION=${VERSION/-SNAPSHOT/-0.${GIT_HASH}.$(date +%04Y%02m%02d%02H%02M)}
-IFS='-' read -ra VERSION <<< "$VERSION"
-RELEASE=${VERSION[1]-1}
-MILESTONE=master
-
-# GH RPM builds will be used only for OST so Firefox and Chrome are enough
-# GWT build memory needs to be limited
-EXTRA_BUILD_FLAGS=""
-EXTRA_BUILD_FLAGS="${EXTRA_BUILD_FLAGS} --no-transfer-progress"
-EXTRA_BUILD_FLAGS="${EXTRA_BUILD_FLAGS} -Dgwt.userAgent=gecko1_8,safari"
-EXTRA_BUILD_FLAGS="${EXTRA_BUILD_FLAGS} -Dgwt.compiler.localWorkers=1"
-EXTRA_BUILD_FLAGS="${EXTRA_BUILD_FLAGS} -Dgwt.jvmArgs='-Xms1G -Xmx3G'"
-
-export MAVEN_OPTS="-Xms1G -Xmx2G"
-
-# Set the location of the JDK that will be used for compilation:
-export JAVA_HOME="${JAVA_HOME:=/usr/lib/jvm/java-11}"
-
-[ -d ${ARTIFACTS_DIR} ] || mkdir -p ${ARTIFACTS_DIR}
-[ -d rpmbuild/SOURCES ] || mkdir -p rpmbuild/SOURCES
-
-make validations
+# Create RPM build directories
+export top_dir="${PWD}/rpmbuild"
+test -d "${top_dir}" && rm -rf "${top_dir}" || :
+mkdir -p "${top_dir}/SOURCES"
 
 # Get the tarball
 make dist
 mv *.tar.gz rpmbuild/SOURCES
 
-# create the src.rpm
+# Create the src.rpm
 rpmbuild \
-    -D "_topdir rpmbuild" \
+    -D "_topdir ${top_dir}" \
+    -D "release_suffix ${RELEASE}" \
     -ts rpmbuild/SOURCES/*.gz
--- version.mak
+++ version.mak
@@ -17,16 +17,16 @@
 #
 MILESTONE_IF_NEEDED=master
 
-# RPM_RELEASE_ON_RELEASE should be set to the rpm release to have on release (non-SNAPSHOT) builds.
-RPM_RELEASE_ON_RELEASE=1
-
 # AUTO_MILESTONE is set to MILESTONE_IF_NEEDED on SNAPSHOT builds, empty otherwise.
 AUTO_MILESTONE=$(shell cat pom.xml | head -n 20 | grep '<version>' | head -n 1 | sed -e 's/.*>\(.*\)<.*/\1/' | grep -q 'SNAPSHOT$$' && echo $(MILESTONE_IF_NEEDED))
 
+MILESTONE?=$(AUTO_MILESTONE)
+
+# RPM_RELEASE_ON_RELEASE should be set to the rpm release to have on release (non-SNAPSHOT) builds.
+RPM_RELEASE_ON_RELEASE=1
+
 # AUTO_RPM_RELEASE is set to 0.0.something on SNAPSHOT builds, and to RPM_RELEASE_ON_RELEASE otherwise.
 AUTO_RPM_RELEASE=$(shell if cat pom.xml | head -n 20 | grep '<version>' | head -n 1 | sed -e 's/.*>\(.*\)<.*/\1/' | grep -q 'SNAPSHOT$$'; then echo 0.2.$(MILESTONE).$$(date -u +%Y%m%d%H%M%S); else echo $(RPM_RELEASE_ON_RELEASE); fi)
-
-MILESTONE=$(AUTO_MILESTONE)
 
 # RPM release should be automatic. If needed to be set manually:
 # For pre-release:
